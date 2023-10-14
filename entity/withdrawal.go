bf1b20e178b2dd6477cfb5eadcf830936dc9761e
Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-10-14 14:28:12 +0700

    add withdrawal features

--- entity/withdrawal.go
+++ entity/withdrawal.go
@@ -7,7 +7,7 @@ type Withdrawal struct {
 	ID_User uint64    `gorm:"type:int(100);index" json:"id_user"`
 	User    User      `gorm:"foreignKey:ID_User" json:"-"`
 	Date    time.Time `gorm:"type:datetime;default:current_timestamp" json:"date"`
-	Ammount uint64    `gorm:"type:int(100)" json:"ammount"`
+	Amount  uint64    `gorm:"type:int(100)" json:"amount"`
 	To      string    `gorm:"type:varchar(255);not null" json:"to"`
 	Status  uint64    `gorm:"type:int(100);default:1" json:"status"`
 }
