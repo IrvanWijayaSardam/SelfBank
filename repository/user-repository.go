c0d41630062ee2a8d31945df8e44eb56460473eb
Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-10-29 10:31:33 +0700

    fix: bug user password changed when upload profile

--- repository/user-repository.go
+++ repository/user-repository.go
@@ -48,14 +48,6 @@ func (db *userConnection) InsertUser(user entity.User) entity.User {
 }
 
 func (db *userConnection) UpdateUser(user entity.User) entity.User {
-	if user.Password != "" {
-		user.Password = hashAndSalt([]byte(user.Password))
-	} else {
-		var tempUser entity.User
-		db.connection.Find(&tempUser, user.ID)
-		user.Password = tempUser.Password
-	}
-
 	db.connection.Save(&user)
 	return user
 }
