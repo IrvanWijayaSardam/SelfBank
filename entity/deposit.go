4abbf573e1276a3b1880497e64377e017338a025
Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-10-30 17:05:45 +0700

    set default date from golang

--- entity/deposit.go
+++ entity/deposit.go
@@ -6,7 +6,7 @@ type Deposit struct {
 	ID      string    `gorm:"primary_key" json:"id"`
 	ID_User uint64    `gorm:"type:int(100);index" json:"id_user"`
 	User    User      `gorm:"foreignKey:ID_User" json:"-"`
-	Date    time.Time `gorm:"type:datetime;default:current_timestamp" json:"date"`
+	Date    time.Time `gorm:"type:datetime" json:"date"`
 	Amount  uint64    `gorm:"type:int(100)" json:"amount"`
 	Status  uint64    `gorm:"type:int(100);default:1" json:"status"`
 }
